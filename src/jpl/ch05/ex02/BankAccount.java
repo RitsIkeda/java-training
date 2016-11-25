630f54cfb024de6eee23e221b32e89372a3023bf
RitsIkeda <Akinobu.Ikeda@jrits.ricoh.co.jp>
2016-11-25 22:17:18 +0900

    2016/11/25

---
+++ src/jpl/ch05/ex02/BankAccount.java
@@ -0,0 +1,41 @@
+
+package jpl.ch05.ex02;
+
+import java.util.*;
+
+public class BankAccount {
+    private long number;
+    private long balance;
+    private Action lastAct;
+    private static History history; /* 処理のヒストリー */
+
+    public class Action {
+
+    }
+    /* Actionをメンバとしてもつ BankAccount専用のヒストリークラスであるため、
+      BankAccountのネストしたクラスで提供する。
+      1つのBankAccountのインスタンスにつき、1つのHisotyをもつためstaticとする。
+      */
+     public static class History  {
+         LinkedList <Action> actions = new LinkedList <Action>();
+         private void addAction(Action action) {
+             actions.push(action);
+             if( actions.size() > 10) {
+                 actions.pollLast();
+             }
+         }
+         public Action getAction(int index) {
+             if(index < 10) {
+                 return actions.get(index);
+             } else {
+                 return null;
+             }
+         }
+         public Action next() {
+             return getAction(1);
+         }
+    }
+    public History history() {
+        return history;
+    }
+}
